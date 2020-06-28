e363df08698d9d49917456b5463fd9d2fd10a5ab
unknown <j.j.reidy@gmail.com>
2020-06-28 16:58:18 +0100

    Added projects to define roles within service

---
+++ QuizMaster.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace QuizMaster.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ QuizMasterApi/Controllers/QuizController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QuizMasterApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QuizController : ControllerBase
+    {
+        private readonly ILogger<QuizController> _logger;
+
+        public QuizController(ILogger<QuizController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("random")]
+        public IEnumerable<QuizController> GetRandomQuestion()
+        {
+            return null;
+        }
+    }
+}
